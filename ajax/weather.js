3ca4c4b2d84a5c06fb7a4ff06267ee00b4f7fbfa
Michelle <cohenmic@oregonstate.edu>
2018-02-17 23:26:41 -0800

    working with zip only

--- ajax/weather.js
+++ ajax/weather.js
@@ -5,32 +5,27 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 function bindButtons(){
   document.getElementById('submitWx').addEventListener('click',function(event){
     getWeatherFromZip(document.getElementById('wxForm').elements.zipCode.value);
-    console.log("zip code is "+document.getElementById('wxForm').elements.zipCode.value);
     event.preventDefault();
   });
 }
 
 function getWeatherFromZip(zip){
-  console.log("called getWx from zip");
   var apiKey = 'fa7d80c48643dfadde2cced1b1be6ca1';
   var req = new XMLHttpRequest();
   zip = String(zip);
-  console.log("zip is "+zip);
   var url = 'https://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&appid='+apiKey;
-  console.log("url is "+url);
   req.open('GET',url,true);
-  req.setRequestHeader('Content-Type','application/json');
-//  req.send(null); 
+  //req.setRequestHeader('Content-Type','application/json');
   req.addEventListener('load',function(){
     console.log("loaddeeed");
     if(req.status>=200 && req.status<400){
-      console.log("loaded yay");
       var json = JSON.parse(req.responseText);
       document.getElementById('locName').textContent = "Location Name: "+json.name;
       document.getElementById('tempValue').textContent = "Temperature: "+kToF(json.main.temp)+"F";
       document.getElementById('windVal').textContent = "Wind Speed: "+json.wind.speed+" MPH";
     }
   });
+  req.send(null); 
 }
  
 function kToF(temp){
