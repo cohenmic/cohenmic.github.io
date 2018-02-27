4305007564796facd072406db01eb8a26c85045a
Michelle <cohenmic@oregonstate.edu>
2018-02-27 12:53:28 -0800

    webpage first draft
    webpage

---
+++ webpage/carousel.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', createCarousel);
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('clickMe').addEventListener('click', function(){
+    console.log("you shouldn't have done that!");
+    document.body.querySelector('main').innerHTML ="Yoooo";
+    document.getElementById('clickMe').addEventListener('click', function(){
+      createCarousel();
+      console.log("better now!");
+    });
+  });
+});
+
+var main = document.body.querySelector("main");
+
+var myImgs = ["images/boulderCreekLakeSlide.jpg","images/canyonCreekSlide.jpg","images/grizzlyLakeSlide.jpg"];
+var imgLinks = [ , , ];
+
+function carousel(dest,idx){
+   if (idx >= myImgs.length)
+    idx=0;
+  dest.src = myImgs[idx];
+  idx +=1;
+ // t=setTimeout(function(){carousel(dest,idx)},1000);
+  setTimeout(function(){carousel(dest,idx)},4000);
+}
+
+function createCarousel(){
+  var slideSection = document.createElement("section");
+  slideSection.setAttribute("id","slideSection");
+  var theSlide = document.createElement("img");
+  theSlide.setAttribute("id","theSlide");
+  slideSection.appendChild(theSlide);
+  main.innerHTML = "";
+  main.appendChild(slideSection);
+  carousel(theSlide,0);
+}
+
+
---
+++ webpage/main.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+  var buttonDrops = document.querySelectorAll("li.dropdown");
+  for (var i=0;i<buttonDrops.length;i++)
+    buttonDrops[i].addEventListener("click", toggleDropdown);
+
+  //make dropdowns collapse when clicking away from navbar
+  var topNav = document.getElementById('topNav');
+  //topNav.addEventListener('click', function(e){
+  document.body.addEventListener('click', function(e){
+    if (!isInside(e.target, topNav))
+      clearDropdowns();
+  });
+}
+
+function toggleDropdown(e){
+  //save current status in a variable, then hide em all, then show current, or do nothing
+  
+	console.log("should be showing list");
+  var currentExpanded;
+  if (e.target.querySelector("ul.dropdownContent").getAttribute('data-expanded') == "true")
+    currentExpanded = true;
+  else
+    currentExpanded = false;
+
+  var children = e.target.parentElement.querySelectorAll('li.dropdown > ul.dropdownContent'); //target is the li, then parent is the ul (horizontal)
+  for (var i=0;i<children.length;i++){
+      children[i].setAttribute("data-expanded","false");
+      children[i].style.display = "none";
+  }
+  //now deal with the current one
+  if(currentExpanded){
+    e.target.querySelector("ul.dropdownContent").style.display = "none";
+    e.target.querySelector("ul.dropdownContent").setAttribute("data-expanded","false");
+  }
+  else{
+    e.target.querySelector("ul.dropdownContent").style.display = "block";
+    e.target.querySelector("ul.dropdownContent").setAttribute("data-expanded","true");
+  }
+}
+function clearDropdowns(){
+  //save current status in a variable, then hide em all, then show current, or do nothing
+  
+	console.log("clearing dropdowns");
+  var dropdowns = document.querySelectorAll("ul.dropdownContent");
+  for (var i=0;i<dropdowns.length;i++){
+    dropdowns[i].style.display = "none";
+    dropdowns[i].setAttribute("data-expanded","false");
+  }
+}
+/*see if node is inside target.
+* this function taken from Eloquent Javascript, page 243 */
+function isInside(node,target){
+  for(; node!=null;node=node.parentNode)
+    if(node==target)
+      return true;
+  //return false;
+}
+
